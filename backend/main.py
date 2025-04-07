8fc32915a97b724c4f2eae863a0b483d06321c61
Didenko Victoria <155799784+misfits3z@users.noreply.github.com>
2025-04-07 17:27:01 +0800

    added CORS

--- backend/main.py
+++ backend/main.py
@@ -1,10 +1,25 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import Response
 from src.api import utils, contacts, auth_router, users
 import os
 
-
 app = FastAPI(debug=True)
+
+# 👇 Дозволяємо CORS
+origins = [
+    "http://localhost:3000",
+    "http://127.0.0.1:3000",
+    # Додати тут деплой, наприклад:
+    # "https://your-frontend.com"
+]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.get("/")
@@ -16,6 +31,8 @@ async def read_root():
 async def favicon():
     return Response(status_code=204)
 
+
+# 👇 Роутери
 app.include_router(auth_router.router, prefix="/auth")
 app.include_router(utils.router, prefix="/api")
 app.include_router(contacts.router, prefix="/api")
